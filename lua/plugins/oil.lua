175fc45871f1ecfea701882e5e399508a3ed08e5
Jack Hensley <jack.hensley@kimley-horn.com>
2026-02-05 11:20:04 -0500

    oil, copilot

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,7 @@
+-- Oil.nvim - file explorer that lets you edit your filesystem like a buffer
+return {
+	'stevearc/oil.nvim',
+	opts = {},
+	-- Optional dependencies
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
